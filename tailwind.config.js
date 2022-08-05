90bbc85e355004e42f43b2e0ffa8984d2d75384a
joel <daniel_211211@yahoo.com.mx>
2022-08-04 21:36:32 -0500

    Avances diseño

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
         'negro': 'rgb(35, 31 , 32)',
         'blanco': 'rgb(255, 255, 255)',
       },
+      screens: {
+        'sm': '576px',
+        // => @media (min-width: 576px) { ... }
+  
+        'md': '960px',
+        'md2': '1044px',
+        // => @media (min-width: 960px) { ... }
+  
+        'lg': '1200px',
+        // => @media (min-width: 1440px) { ... }
+      },
     },
   },
   plugins: [],
